7bd56e7bded391c70b9f27e943a9f6f86f511c42
Guilherme Braga <crazyboysfh3@gmail.com>
2023-05-24 17:32:21 -0300

    Códigos estão em codes

--- Codes/byeTrash.py
+++ Codes/byeTrash.py
@@ -2,8 +2,8 @@ import os
 
 a = 0
 os.chdir('/home/solas/Documentos/Solas/Programming/Python/testZone')
-for foldername, subfolders, filenames in os.walk('.'):
+for foldername, subfolders, filenames in os.walk('..'):
     for filename in filenames:
-        a += os.path.getsize(os.path.join('.', filename))
+        a += os.path.getsize(os.path.join('..', filename))
 
 print(a/1024/1024, 'MB')
