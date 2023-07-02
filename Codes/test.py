8f2219135d08ab769471c1c4754f3b3a51663d67
Guilherme Braga <89932943+S0LA1R3@users.noreply.github.com>
2023-07-02 18:55:53 -0300

    Add files via upload

---
+++ Codes/test.py
@@ -0,0 +1,9 @@
+import pytesseract
+import numpy as np
+import cv2
+
+img = cv2.imread(r'C:\Users\Madona\Desktop\GuilhermeEstudas\OCRPython\Imagens\teste01.jpg')
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+cv2.imshow('imagem', img)
+texto = pytesseract.image_to_string(rgb)
+print(texto)
---
+++ Codes/test8.py
@@ -0,0 +1,10 @@
+import cv2
+import pytesseract
+import re
+from pytesseract import Output
+
+img = cv2.imread(r'.\Imagens\tabela_teste.jpg')
+rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
+configTesseract = '--tessdata-dir tessdata'
+resultado = pytesseract.image_to_data(rgb, config=configTesseract, lang='por', output_type=Output.DICT)
+
