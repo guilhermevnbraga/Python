85584f1d619f07488c4cf46b45bda317e1d91429
Guilherme Braga <crazyboysfh3@gmail.com>
2023-07-04 22:39:38 -0300

    update

--- ComputerVision/OCRPython/test.py
+++ ComputerVision/OCRPython/test.py
@@ -2,7 +2,7 @@ import pytesseract
 import numpy as np
 import cv2
 
-img = cv2.imread(r'.\Imagens\teste01.jpg')
+img = cv2.imread(r'C:\Users\PESSOAL\Documents\GitHub\Python\ComputerVision\OCRPython\Imagens\teste01.jpg')
 rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 cv2.imshow('imagem', img)
 texto = pytesseract.image_to_string(rgb)
--- ComputerVision/OCRPython/test2.py
+++ ComputerVision/OCRPython/test2.py
@@ -6,6 +6,6 @@ img = cv2.imread(r'C:\Users\PESSOAL\Documents\GitHub\Python\ComputerVision\OCRPy
 #cv2.imshow('imagem', img)
 rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 cv2.imshow('imagem', rgb)
-configTesseract = '--tessdata-dir tessdata'
+configTesseract = r'--tessdata-dir C:\Users\PESSOAL\Documents\GitHub\Python\ComputerVision\OCRPython\tessdata'
 texto = pytesseract.image_to_string(rgb, lang='por', config=configTesseract)
 print(texto)
