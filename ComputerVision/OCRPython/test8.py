0e2e8a83f84c312551a12b3b85a370b753b9bdfa
Guilherme Braga <crazyboysfh3@gmail.com>
2023-07-04 23:00:54 -0300

    update

--- ComputerVision/OCRPython/test8.py
+++ ComputerVision/OCRPython/test8.py
@@ -3,8 +3,8 @@ import pytesseract
 import re
 from pytesseract import Output
 
-img = cv2.imread(r'.\Imagens\tabela_teste.jpg')
+img = cv2.imread(r'C:\Users\PESSOAL\Documents\GitHub\Python\ComputerVision\OCRPython\Imagens\tabela_teste.jpg')
 rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
-configTesseract = '--tessdata-dir tessdata'
+tessdataDir = r'C:\Users\PESSOAL\Documents\GitHub\Python\ComputerVision\OCRPython\tessdata'
+configTesseract = f'--tessdata-dir "{tessdataDir}"'
 resultado = pytesseract.image_to_data(rgb, config=configTesseract, lang='por', output_type=Output.DICT)
-
