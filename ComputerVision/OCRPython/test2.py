0f342711a361df7632ae03009a06e28470d540dd
Guilherme Braga <crazyboysfh3@gmail.com>
2023-07-04 22:37:25 -0300

    update

--- ComputerVision/OCRPython/test2.py
+++ ComputerVision/OCRPython/test2.py
@@ -2,7 +2,7 @@ import pytesseract
 import cv2
 from time import sleep
 
-img = cv2.imread(r'\Imagens\teste02.jpg')
+img = cv2.imread(r'C:\Users\PESSOAL\Documents\GitHub\Python\ComputerVision\OCRPython\Imagens\teste02.jpg')
 #cv2.imshow('imagem', img)
 rgb = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
 cv2.imshow('imagem', rgb)
