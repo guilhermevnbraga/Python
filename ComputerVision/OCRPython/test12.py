b43112ab39e4a417b94478895a5ad86b8c164ba7
Guilherme Braga <crazyboysfh3@gmail.com>
2023-07-05 23:37:54 -0300

    update

--- ComputerVision/OCRPython/test12.py
+++ ComputerVision/OCRPython/test12.py
@@ -1,1 +1,19 @@
 import cv2
+
+img = cv2.imread(r'C:\Users\PESSOAL\Documents\GitHub\Python\ComputerVision\OCRPython\Imagens\trecho-livro.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+val, otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+cv2.imshow('imagem', otsu)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+print(val)
+
+img = cv2.imread(r'C:\Users\PESSOAL\Documents\GitHub\Python\ComputerVision\OCRPython\Imagens\receita01.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+val, otsu = cv2.threshold(gray, 0, 255, cv2.THRESH_BINARY | cv2.THRESH_OTSU)
+cv2.imshow('imagem', otsu)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+print(val)
