290467e67287eb06340e719bc025ba5abee303ec
Guilherme Braga <crazyboysfh3@gmail.com>
2023-07-05 23:55:29 -0300

    update

---
+++ ComputerVision/OCRPython/test15.py
@@ -0,0 +1,12 @@
+import cv2
+
+img = cv2.imread(r'C:\Users\PESSOAL\Documents\GitHub\Python\ComputerVision\OCRPython\Imagens\img-process.jpg')
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+cv2.imshow('imagem', 255-gray)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+
+val, thresh = cv2.threshold(gray, 140, 255, cv2.THRESH_BINARY_INV)
+cv2.imshow('imagem', thresh)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
