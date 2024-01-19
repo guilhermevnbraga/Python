54c032c9f5cdf0bc3ef8b1af69fa91270af6c7de
Guilherme Braga <crazyboysfh3@gmail.com>
2024-01-19 19:25:39 -0300

    update

---
+++ Codes/amogus.py
@@ -0,0 +1,39 @@
+# print
+
+print('Hello, world', end='')
+print('asjdfaiso', 'ajsdoas')
+
+
+# variaveis
+
+a = 'ola'
+b = 123
+c = 1.1
+d = True
+
+print(f'123 é {b}')
+
+'''
+Operadores basicos
++
+-
+*
+/
+
+operadores 2
+
+** potencia
+//
+%
+
+5//2
+5%2 = 1
+
+operador 3
+()
+
+1 + 5 % 2 * 3
+
+'''
+
+print(1 + 5 % 2 * 3)
